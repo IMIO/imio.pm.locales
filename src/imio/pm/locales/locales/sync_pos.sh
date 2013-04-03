198598899991d008ca0f725fd84d1c89d5f7a3ba
Gauthier Bastien <g.bastien@imio.be>
2013-04-03 09:44:34 +0000

    Use symlink to i18ndude

--- src/imio/pm/locales/locales/sync_pos.sh
+++ src/imio/pm/locales/locales/sync_pos.sh
@@ -3,7 +3,7 @@ languages="de en es fr nl"
 
 for file in $files; do
     for language in $languages; do
-        /srv/python/python273/bin/i18ndude sync --pot $file.pot $language/LC_MESSAGES/$file.po
+        i18ndude sync --pot $file.pot $language/LC_MESSAGES/$file.po
         msgfmt -o $language/LC_MESSAGES/$file.mo $language/LC_MESSAGES/$file.po
     done
 done
