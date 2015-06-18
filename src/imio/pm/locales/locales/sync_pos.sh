36909ce56af5c465e8d44f116d2291e97ddcf6d3
Gauthier Bastien <g.bastien@imio.be>
2015-06-18 13:57:36 +0000

    Added translations for faceted navigation widgets

--- src/imio/pm/locales/locales/sync_pos.sh
+++ src/imio/pm/locales/locales/sync_pos.sh
@@ -1,4 +1,4 @@
-files="PloneMeeting plone imio.actionspanel datagridfield imio.history collective.eeafaceted.z3ctable"
+files="PloneMeeting plone imio.actionspanel datagridfield imio.history collective.eeafaceted.z3ctable eea"
 languages="de en es fr nl"
 
 for file in $files; do
