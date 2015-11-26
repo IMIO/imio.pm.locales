d9af2ec499181a06c00192b91793aa558bae93fa
Gauthier Bastien <g.bastien@imio.be>
2015-11-26 11:50:40 +0000

    Override talcondition descr to specify that 'tool' and 'cfg' are available too

--- src/imio/pm/locales/locales/sync_pos.sh
+++ src/imio/pm/locales/locales/sync_pos.sh
@@ -1,4 +1,4 @@
-files="PloneMeeting plone imio.actionspanel datagridfield imio.history collective.eeafaceted.z3ctable eea"
+files="PloneMeeting plone imio.actionspanel datagridfield imio.history collective.eeafaceted.z3ctable eea collective.behavior.talcondition"
 languages="de en es fr nl"
 
 for file in $files; do
