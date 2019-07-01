3721212602c321c791a255f999beb4450bb6409e
Gauthier Bastien <g.bastien@imio.be>
2019-07-01 10:40:11 +0200

    Back to development: 4.1rc3

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,6 +2,12 @@ Changelog
 =========
 
 
+4.1rc3 (unreleased)
+-------------------
+
+- Nothing changed yet.
+
+
 4.1rc2 (2019-07-01)
 -------------------
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '4.1rc2'
+version = '4.1rc3.dev0'
 
 setup(name='imio.pm.locales',
       version=version,
