1871c41d2c35dd94444c2543d812b48e9c78f4b6
Gauthier Bastien <gauthier.bastien@imio.be>
2024-10-02 16:08:34 +0200

    Back to development: 4.2.20.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from io import open
 from setuptools import setup, find_packages
 import os
 
-version = '4.2.20.1'
+version = '4.2.20.2.dev0'
 
 setup(name='imio.pm.locales',
       version=version,
