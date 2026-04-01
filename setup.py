f0a0b34d34fbd7f6b375167854bfa7600efc46c6
Gauthier Bastien <gauthier.bastien@imio.be>
2026-04-01 13:32:39 +0200

    Back to development: 4.2.34rc6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from io import open
 from setuptools import setup, find_packages
 import os
 
-version = '4.2.34rc5'
+version = '4.2.34rc6.dev0'
 
 setup(name='imio.pm.locales',
       version=version,
